8d115e6835b1e777bae85e949c23ff65d89b848f
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-04-12 12:06:08 +0530

    Update urgent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 // import ChatSpace from "./components/ChatPlace";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/vanilla/login";
-import Chat from "./components/vanilla/chat";
+import Chat from "./components/vanilla/newchat";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="" element={<Login />} />
-        <Route path="roomID/:id" element={<Chat />} />
+        <Route path="/roomID" element={<ChatApp />} />
       </Routes>
     </>
   );
