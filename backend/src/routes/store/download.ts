2fc9f7c2e2c64db70d101f2f2c6978353a2a16b0
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-04-30 13:49:06 +0000

    implementing local storage import export get set class

--- backend/src/routes/store/download.ts
+++ backend/src/routes/store/download.ts
@@ -1,4 +1,5 @@
 import { writeFile } from "fs";
+import { join } from "path";
 
 class Download {
   private data: String;
@@ -10,11 +11,25 @@ class Download {
     this.data = data;
     localStorage.setItem(data, `chat-at-${Date.now().toString()}.json`);
   }
+  getDownload(data: string) {
+    this.data = data;
+    localStorage.setItem(data, `chat-at-${Date.now().toString()}.json`);
+  }
 
   import(){
 
   }
-  export(){
-    
+  async export() {
+    const fileName = `chat-at-${Date.now().toString()}.json`;
+    const filePath = join(__dirname, fileName);
+    try {
+      await writeFile(filePath, this.data, 'utf8'); // problem here
+      console.log(`Data exported to ${filePath}`);
+    } catch (error) {
+      console.error('Failed to export file:', error);
+    }
   }
 }
+
+
+export const localStore = new Download()
